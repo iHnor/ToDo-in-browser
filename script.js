85667d5ad5519294eb16ad0d0692e886c667e332
Ihor Mykhno <igormykhno@gmail.com>
2021-08-31 10:06:54 +0300

    Fix project after Sasha`s commit

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-const inc = (index = 0) => () => ++index
-const genId = inc()
-
-// let tasks = [
-//     {id: genId(), title: 'Описать массив задач в JavaScript', done: false, description: 'Add Some descript', date: new Date('2021-11-10')},
-//     {id: genId(), title: 'Создать базовый макет страницы для вывода задач', done: false, description: 'Just task', date: new Date('2021-08-20')},
-//     {id: genId(), title: 'Реализовать функцию добавления HTML тэгов для вывода информации о задаче', done: false, date: new Date('2021-08-01')},
-//     {id: genId(), title: 'Ще якась задачка', done: false, date: new Date('2021-08-01')}
-    
-// ];
-
 let contactsElement = document.getElementById('tasks');
 
 function createTaskDiv(task) {
@@ -35,15 +24,15 @@ function createTaskDiv(task) {
     taskBase.appendChild(checkboxAndTask);
     taskBase.appendChild(deleteButton)
 
-   
+
     let description = createDescription(task);
-    taskDescription.appendChild(description);   
-    
-    if(task.date !== undefined){
+    taskDescription.appendChild(description);
+
+    if (task.date !== undefined) {
         let taskDate = createDate(task);
         taskDescription.appendChild(taskDate);
     }
-   
+
     tasksList.appendChild(taskBase);
     tasksList.appendChild(taskDescription);
 
@@ -58,9 +47,8 @@ function createTaskDiv(task) {
 
 function createTitle(task) {
     let title = document.createElement('label');
-    if (task.done === true){
-        title.style.textDecoration = 'line-through'
-        title.style.color = '#C0C0C0'
+    if (task.done) {
+        title.classList.add('taskDone');
     }
     title.innerHTML = task.title;
     return title;
@@ -83,9 +71,9 @@ function createDeletebutton() {
     return button;
 }
 
-function createDescription(task){
+function createDescription(task) {
     let descript;
-    if(task.description !== undefined){
+    if (task.description !== undefined) {
         descript = document.createElement('p');
         descript.innerHTML = task.description;
     }
@@ -96,76 +84,79 @@ function createDescription(task){
     return descript;
 }
 
-function createDate(task){
+function createDate(task) {
     let date = document.createElement('p');
     date.id = 'date'
-    if (task.date < (new Date()))
-        date.style.color = 'red';
-    if(typeof(task.date) !== '')
+    if (new Date(task.date) < new Date()) {
+        date.classList.add('lastDate');
+    }
+
+    if (typeof (task.date) !== '')
         task.date = new Date(task.date)
     date.innerHTML = task.date.toLocaleDateString('uk');
     return date;
 }
 
 
-function deleteTodo(){
+function deleteTodo() {
     let div = this.parentNode
 
-    fetch(tasksEnpoint+'/'+div.parentNode.id, {
+    fetch(tasksEnpoint + '/' + div.parentNode.id, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify()
     })
-
-    div.parentNode.remove();
+    .then(res => {
+        if(res.ok){
+            div.parentNode.remove();
+        }
+    })
+    
 }
 
-function changeStatus(){
+function changeStatus() {
     let label = this.parentNode;
     let checkedBox = label.firstChild;
     let text = label.lastChild;
-    let hide = document.getElementById("hideBtn"); 
-
-    if (checkedBox.checked){
-        text.style.textDecoration = 'line-through'
-        text.style.color = '#C0C0C0'
-        if(hide.disabled){
-            document.getElementById(`${tasks[tasksID].id}`).remove()
+    let hide = document.getElementById("hideBtn");
+    if (checkedBox.checked) {
+        text.classList.add('taskDone');
+        if (hide.disabled) {
+            label.parentNode.parentNode.remove()
         }
     }
     else {
-        label.lastChild.style.textDecoration = 'none'
-        text.style.color = 'rgb(61, 61, 61)'
+        text.classList.remove('taskDone');
     }
 
-    fetch(tasksEnpoint+'/'+checkedBox.id, {
+    fetch(tasksEnpoint + '/' + checkedBox.id, {
         method: 'PATCH',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({done: checkedBox.checked})
+        body: JSON.stringify({ done: checkedBox.checked })
     })
 }
 
-function showDone(){
+function showDone() {
     removeTasks();
 
-    fetch(tasksEnpoint) 
+    fetch(tasksEnpoint)
         .then(response => response.json())
         .then(tasks => tasks.forEach(createTaskDiv));
 
     buttonStatus();
 }
 
-function hideDone(){
+function hideDone() {
     removeTasks();
-    
-    fetch(tasksEnpoint) 
+
+    fetch(tasksEnpoint)
         .then(response => response.json())
         .then(tasks => tasks.forEach((element) => {
-            if (element.done === false){
+            if (element.done === false) {
                 createTaskDiv(element)
             }
         }));
@@ -173,53 +164,55 @@ function hideDone(){
     buttonStatus();
 }
 
-function removeTasks(){
+function removeTasks() {
     let div = document.querySelectorAll('.task');
     div.forEach((elem) => {
         elem.remove()
     })
 }
 
-function buttonStatus(){
+function buttonStatus() {
     let show = document.getElementById("showBtn");
-    let hide = document.getElementById("hideBtn"); 
+    let hide = document.getElementById("hideBtn");
     show.disabled = !show.disabled;
     hide.disabled = !hide.disabled;
-    
+
 }
 
-const addTaskForm =  document.forms['addTask'];
+const addTaskForm = document.forms['addTask'];
 
-function createContact(formData){
+function createTask(formData) {
     let contact = Object.fromEntries(formData.entries())
-    let newTask = {id: genId(), title: contact.title, done: false, description: contact.description, date: new Date(contact.date)};
+    let newTask = { title: contact.title, done: false, description: contact.description, date: new Date(contact.date) };
     return newTask;
 }
- 
+
 const tasksEnpoint = 'http://localhost:3000/tasks'
 
 addTaskForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const formData = new FormData(addTaskForm);
-    const contact = createContact(formData);
+    const task = createTask(formData);
 
-    postTask(contact)
-        .then(response => response.json())
-        .then(createTaskDiv)
-    
-    addTaskForm.reset();
+    postTask(task)
+        .then(task => {
+            createTaskDiv(task)
+            addTaskForm.reset();
+        })
 });
 
-function postTask(contact) {
+function postTask(task) {
     return fetch(tasksEnpoint, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(contact)
+        body: JSON.stringify(task)
     })
+    .then(response => response.json())
+    
 }
 
-fetch(tasksEnpoint) 
+fetch(tasksEnpoint)
     .then(response => response.json())
     .then(tasks => tasks.forEach(createTaskDiv));
